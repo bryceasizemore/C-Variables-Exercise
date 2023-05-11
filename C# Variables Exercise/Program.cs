dcc785279c45f683c927be8435a997411eed299e
bryce sizemore <brxccce@outlook.com>
2023-05-10 17:30:24 -0700

    Add project files.

---
+++ C# Variables Exercise/Program.cs
@@ -0,0 +1,15 @@
+﻿// declare and initialize variables 
+string name = "Hannah";
+int age = 20;
+char middleInitial = 'E';
+bool isOver18 = true;
+double currentTemp = 90.1; 
+decimal currenetPrice = 49.99m;
+// Writing variables to the console
+Console.WriteLine($"name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currenetPrice}"); 
+// done 
